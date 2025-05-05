eda430edd939534bcb531314ef44c56e14aedc12
Subhashree4307 <mohantysubhashree2005@gmail.com>
2025-05-05 13:49:41 +0530

    3rd commit

---
+++ Expense-Tracker/src/components/main/Form/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles, RadioGroup } from "@mui/material";
+export default makeStyles(()=>({
+    radioGroup: {
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: '-10px',
+      },
+      button: {
+        marginTop: '20px',
+      },
+}))
